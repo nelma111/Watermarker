d9276f09796dd904a8a5d309b8c79b4a01e65549
nelma111 <max.nell@usz.ch>
2021-01-14 10:43:24 +0100

    Version 0.0.1 alpha
    Added All Buttons as I mentioned in the Mockup Storyboard

---
+++ app/src/main/java/ch/zli/watermarker/ChooseWatermark.java
@@ -0,0 +1,14 @@
+package ch.zli.watermarker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseWatermark extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_watermark);
+    }
+}
---
+++ app/src/main/java/ch/zli/watermarker/SaveAndShare.java
@@ -0,0 +1,14 @@
+package ch.zli.watermarker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaveAndShare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_and_share);
+    }
+}
